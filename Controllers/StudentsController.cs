f796e0e33ff577442ec81f9ceea46ad4abf92687
nitinsamdyan1 <156529259+nitinsamdyan1@users.noreply.github.com>
2024-07-31 10:26:18 -0400

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Data;
+using StudentManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+
+        // Other actions (Create, Edit, Delete) can be added here
+    }
+}
---
+++ data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Models;
+using System.Collections.Generic;
+
+namespace StudentManagement.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
